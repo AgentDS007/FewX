ba57533e3db9aa29786a1cea83fd13c58f24bfe4
AgentDS007 <77388529+AgentDS007@users.noreply.github.com>
2021-06-15 18:52:32 +0200

    changed param of  make_stage to match current detectron2 version

--- fewx/modeling/fsod/fsod_roi_heads.py
+++ fewx/modeling/fsod/fsod_roi_heads.py
@@ -93,7 +93,7 @@ class FsodRes5ROIHeads(ROIHeads):
         blocks = make_stage(
             BottleneckBlock,
             3,
-            first_stride=2,
+            stride=2,
             in_channels=out_channels // 2,
             bottleneck_channels=bottleneck_channels,
             out_channels=out_channels,
